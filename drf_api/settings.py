db24bda0dcf99ca7d87b2c95642bba114b4c1f19
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-12-26 14:00:50 +0100

    feat: add Posts model, its serializer, image_filter choice and image validation

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'profiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
---
+++ posts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Posts
+
+
+class PostsSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    is_owner = serializers.SerializerMethodField()
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'The image size is more than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Posts
+        fields = ['id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+                  'created_at', 'updated_at', 'title', 'content', 'image',
+                  'image_filter']
