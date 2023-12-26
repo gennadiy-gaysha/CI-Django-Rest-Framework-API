57b7d10f4b47e258de4c9e3ae641a7d41a45af92
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-12-26 16:09:36 +0100

    feat: add PostsDetail view to retrieve, edit and delete posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, permissions
+
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Posts
 from .serializers import PostsSerializer
 
@@ -8,6 +11,7 @@ from .serializers import PostsSerializer
 class PostsList(APIView):
     serializer_class = PostsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         posts = Posts.objects.all()
         serializer = PostsSerializer(posts, many=True,
@@ -21,3 +25,38 @@ class PostsList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostsDetail(APIView):
+    serializer_class = PostsSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            post = Posts.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Posts.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostsSerializer(post, context={
+            'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostsSerializer(post, data=request.data,
+                                     context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
