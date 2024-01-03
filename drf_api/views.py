9aaff1f988195fe9df0ba8a48222f2a72fdd1bd6
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-03 18:17:53 +0100

    feat: add links with api endpoints to the home page

--- drf_api/views.py
+++ drf_api/views.py
@@ -10,21 +10,33 @@ def root_route(request):
     api_urls = {
         'ProfileList': 'http://127.0.0.1:8000/profiles/',
         'ProfileDetailUpdateDelete': 'http://127.0.0.1:8000/profiles/3/',
-
         'PostsCreateList': 'http://127.0.0.1:8000/posts/',
         'PostsDetailUpdateDelete': 'http://127.0.0.1:8000/posts/3/',
-
         'CommentCreateList': 'http://127.0.0.1:8000/comments/',
         'CommentDetailUpdateDelete': 'http://127.0.0.1:8000/comments/3/',
-
         'LikeCreateList': 'http://127.0.0.1:8000/likes/',
         'LikeDetailUpdateDelete': 'http://127.0.0.1:8000/likes/3/',
+        'Registration': 'http://127.0.0.1:8000/dj-rest-auth/registration/',
+        'Login': 'http://127.0.0.1:8000/dj-rest-auth/login/',
+        'Logout': 'http://127.0.0.1:8000/dj-rest-auth/logout/',
+        'User': 'http://127.0.0.1:8000/dj-rest-auth/user/',
+        'RefreshToken': 'http://127.0.0.1:8000/dj-rest-auth/token/refresh/',
 
-        'Registration': 'http://127.0.0.1:8000/registration/',
-        'Login': 'http://127.0.0.1:8000/login/',
-        'Logout': 'http://127.0.0.1:8000/logout/',
-        'User': 'http://127.0.0.1:8000/user/',
-        'Refresh Token': 'http://127.0.0.1:8000/token/refresh/',
+        'ProfileListHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/profiles/',
+        'ProfileDetailUpdateDeleteHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/profiles/3/',
+        'PostsCreateListHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/posts/',
+        'PostsDetailUpdateDeleteHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/posts/3/',
+        'CommentCreateListHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/comments/',
+        'CommentDetailUpdateDeleteHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/comments/3/',
+        'LikeCreateListHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/likes/',
+        'LikeDetailUpdateDeleteHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/likes/3/',
+        'RegistrationHeroku':
+            'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/dj-rest-auth/registration/',
+        'LoginHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/dj-rest-auth/login/',
+        'LogoutHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/dj-rest-auth/logout/',
+        'UserHeroku': 'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/dj-rest-auth/user/',
+        'RefreshTokenHeroku':
+            'http://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/dj-rest-auth/token/refresh/',
     }
     return Response(api_urls)
 
