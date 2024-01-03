75393f68423585f28bf4a24202475cb7c3177d2e
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-03 17:46:51 +0100

    fix: fix ALLOWED_HOSTS variable name

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV'
 
-ALLOWED_HOSTS = ["127.0.0.1", os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ["127.0.0.1", os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
