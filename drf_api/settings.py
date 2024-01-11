117b8c5e966f65a8eb6fa59be5de44ba74d11abb
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-11 21:17:14 +0100

    fix: fix: amend DEBUG in settings.py #3

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -46,7 +46,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEV') in os.environ
+DEBUG = os.environ.get('DEV')
 
 
 ALLOWED_HOSTS = ["127.0.0.1", os.environ.get('ALLOWED_HOSTS')]
